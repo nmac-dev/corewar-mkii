cc9fc01dc1e23a1681bf860d716793f4621b7da6
nmac-dev <ntwmac.dev@gmail.com>
2022-01-06 20:00:36 +0000

    Added core.cpp

---
+++ main/src/core.cpp
@@ -0,0 +1,14 @@
+/// core.cpp
+
+/** The Core is a simulated memory space for memory
+ *  Each position within the core acts as a memory address for a single instruction
+ *  Addresses are absolute, however warriors use relative addressing (individual instructions)
+ *  Mod math is used for the core, therefore an address that is (< 0) or (> core max). loops round like a circle
+ *      For example: 
+ *          (1 .. x) = core 
+ *               [x] = current position
+ *          (1 2 3 4 5 [6] 7 8), [6]+3 = ([1] 2 3 4 5 6 7 8)
+ */
+
+static const int CORE_MIN = 0,
+                 CORE_MAX = 8000;
