75a9ef213a928499bfd365a975424e4bb4f38faf
nmac-dev <ntwmac.dev@gmail.com>
2022-02-17 08:10:32 +0000

    Updated loading, getters, & data sanitiser

--- main/src/lib/settings.hpp
+++ main/src/lib/settings.hpp
@@ -1,3 +1,9 @@
+/// Handles match setting
+#pragma once
+
+#include "file_loader.hpp"
+#include <unordered_map>
+
 /// Defines: Singleton class to store & retrieve match settings
 class Settings
 {
@@ -9,27 +15,90 @@ public:
         return match_settings;
     }
 
-    void loadSettings(const char *filename);
-    int &core_size() const,
-        &max_rounds() const,
-        &max_cycles() const,
-        &max_processes() const,
-        &max_warrior_len() const,
-        &min_separation() const;
+    void loadSettings();
+    int core_size() const,
+        max_rounds() const,
+        max_cycles() const,
+        max_processes() const,
+        max_warrior_len() const,
+        min_separation() const;
 
 private:
-    int core_size, max_rounds, max_cycles, 
-        max_processes, max_warrior_len, min_separation;
+    const char *filename = "match_settings.txt";
+    const char  comment = '#';
+    
+    /**
+     * @brief Stores the setting values using the parameter name as a key
+     */
+    std::unordered_map<std::string, int> setting_values;
 
     // Singleton instance
     static Settings match_settings;
-    Settings();
+    Settings() {}
+
+    void removeWhiteSpace(std::string &str);
 };
 
-// Defines singleton class instance
+/// Defines singleton class instance
 Settings Settings::match_settings;
 
-void Settings::loadSettings(const char *filename) 
+/**
+ * @brief Uses file_loader:: to read match_settings.txt, then processes the contents into the hash table
+ */
+void Settings::loadSettings() 
 {
-    
+    std::vector<std::string> data = file_loader::getFileData(filename, comment);
+    std::string name;
+    int value;
+    setting_values.clear();
+
+    /// clean data, get setting name & value
+    for (int i = 0; i < data.size(); i++)
+    {
+        removeWhiteSpace(data[i]);
+        for (int j = 0; j < data[i].length(); j++)
+        {
+            if (data[i][j] == '=')
+            {
+                name = data[i].substr(0, j);
+                value = std::stoi(data[i].substr(j +1, data[i].length()));
+                break;
+            }
+        }
+        setting_values.insert(std::make_pair(name, value));
+    }
 }
+
+/**
+ * @brief Shuffles all whitespace to the end of the string and erases the whitespace segment
+ * @param str reference
+ */
+void Settings::removeWhiteSpace(std::string &str)
+{
+    int ws = 0; // whitespace counter
+    for (int i = 0; i < str.length() - ws; i++)
+    {
+        while (str[i + ws] == ' ')
+            ws++;
+        str[i] = str[i + ws];
+    }
+    str.erase(str.length() - ws);
+}
+
+/// number of memory addresses within the core
+int Settings::core_size() const       {return setting_values.find("core_size")->second;}
+
+/// max number of rounds before the game is concluded
+int Settings::max_rounds() const      {return setting_values.find("max_rounds")->second;}
+
+/// max number of cycles before the round has been concluded
+int Settings::max_cycles() const      {return setting_values.find("max_cycles")->second;}
+
+/// max number of processes a single warrior can create
+int Settings::max_processes() const   {return setting_values.find("max_processes")->second;}
+
+/// max instructions a warrior can consist of
+int Settings::max_warrior_len() const {return setting_values.find("max_warrior_len")->second;}
+
+/// min distance between warriors at the start of a round
+int Settings::min_separation() const  {return setting_values.find("min_separation")->second;}
