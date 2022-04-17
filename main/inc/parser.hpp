30e79ddb660ae2857d15f0eb9e248a85b186623d
nmac-dev <ntwmac.dev@gmail.com>
2022-04-17 03:26:32 +0100

    Added: concise types

--- main/inc/parser.hpp
+++ main/inc/parser.hpp
@@ -3,6 +3,8 @@
 
 // #define ASM_PARSER_DEBUG
 
+#define ASM_CDOE_COMMENT ';' // assembly code comment character
+
 #include <unordered_map>
 #include <string>
 #include <vector>
@@ -11,60 +13,67 @@
 namespace Parser
 {
 
-namespace //// Anonymous Namespace
-{
-    using namespace ASM;
-
-    /* Lookup HashTables */
-    
-/// A hash table used as a label linker, storing the label position relative to the warrior
-using LabelLinker  = std::unordered_map<std::string, int>;
 using AssemblyCode = std::vector<std::string>;
 
-/// Hashes an opcode string to get the enum value
-std::unordered_map<std::string, _OP> opcode_tbl =
+namespace //// Anonymous Namespace
 {
-    {"dat", _OP::DAT}, {"mov", _OP::MOV}, {"add", _OP::ADD}, {"sub", _OP::SUB}, 
-    {"mul", _OP::MUL}, {"div", _OP::DIV}, {"mod", _OP::MOD}, {"jmp", _OP::JMP}, 
-    {"jmz", _OP::JMZ}, {"jmn", _OP::JMN}, {"djn", _OP::DJN}, {"cmp", _OP::CMP},  
-    {"slt", _OP::SLT}, {"spl", _OP::SPL}
+using namespace ASM;
+
+/* Lookup HashTables */
+using LabelLinker  = std::unordered_map<std::string, int>;  // stores label name and index
+using Opcodes      = std::unordered_map<std::string, _OP>;
+using Modifiers    = std::unordered_map<std::string, _MOD>;
+using AddrModes    = std::unordered_map<char, _AM>;
+
+/// Hashes an opcode string to get the enum value
+Opcodes opcode_tbl =
+{
+    {"dat", _OP::DAT}, {"mov", _OP::MOV}, 
+    {"cmp", _OP::CMP}, {"slt", _OP::SLT}, {"spl", _OP::SPL},
+    {"add", _OP::ADD}, {"sub", _OP::SUB}, {"mul", _OP::MUL}, {"div", _OP::DIV}, {"mod", _OP::MOD}, 
+    {"jmp", _OP::JMP}, {"jmz", _OP::JMZ}, {"jmn", _OP::JMN}, {"djn", _OP::DJN}
 };
 
 /// Hashes a modifier string to get the enum value
-std::unordered_map<std::string, _MOD> modifier_tbl =
+Modifiers modifier_tbl =
 {
-    {"a", _MOD::A}, {"b", _MOD::B}, {"ab", _MOD::AB}, {"ba", _MOD::BA}, 
-    {"f", _MOD::F}, {"x", _MOD::X}, {"i",  _MOD::I}
+    {"a",  _MOD::A},  {"b",  _MOD::B}, 
+    {"ab", _MOD::AB}, {"ba", _MOD::BA}, 
+    {"f",  _MOD::F},  {"x",  _MOD::X}, 
+    {"i",  _MOD::I}
 };
 
 /// Hashes an addressing mode string to get the enum value
-std::unordered_map<char, _AM> admo_tbl =
+AddrModes admo_tbl =
 {
-    {'#', _AM::IMMEDIATE}, {'$', _AM::DIRECT},     {'*', _AM::A_INDIRECT}, {'@', _AM::B_INDIRECT}, 
-    {'{', _AM::A_PRE_DEC}, {'}', _AM::A_POST_INC}, {'<', _AM::B_PRE_DEC},  {'>', _AM::B_PRE_INC}
+    {'#', _AM::IMMEDIATE},  {'$', _AM::DIRECT},
+    {'*', _AM::INDIRECT_A}, {'@', _AM::INDIRECT_B},
+    {'{', _AM::PRE_DEC_A},  {'<', _AM::PRE_DEC_B},
+    {'}', _AM::POST_INC_A}, {'>', _AM::POST_INC_B} 
 };
 
-    /* Parser Utility Functions */
+/* Utility Functions */
 
-/// Outputs an error for an invalid assembly argument with the full instruction, then triggers an Exception
-/// @param full_inst 
-/// @param asm_arg 
-inline void invalidAssemblyError(std::string full_inst, std::string asm_arg);
+/// Outputs an error for an invalid assembly argument with the full instruction (triggers Exception)
+/// @param line full assembly code instruction
+/// @param asm_arg asssembly code argument relative to the error
+/// @param index line number of assembly instruction
+inline void invalidAssemblyError(std::string line, std::string asm_arg, int index);
 
 /// Validates if: char is an assembly instruction argument seperator [ ' ' | '\\t' | ' . ' | ' , ' ]
 /// @param val char value to check
-/// @return true if char is a number
+/// @return true if char is a seperator
 inline bool isAsmSeperator(char val);
 
 /// Takes an assembly instruction string and returns the next valid argument from the position given 
-/// @param str assembly instruction string
+/// @param line assembly instruction string
 /// @param pos start position to look for the next assembly argument
 /// @return string of the assembly argument
-inline std::string findAsmArgument(std::string str, int &pos);
+inline std::string findAsmArgument(std::string line, int &pos);
 
-/// Cleans instruction string (removes extra inline spaces & resizes string)
-/// @param str assembly instruction as a string
-inline std::string cleanAsmStr(std::string str);
+/// Cleans instruction string, removes extra inline spaces/comments & resizes string
+/// @param line assembly instruction string
+inline std::string cleanAsmStr(std::string line);
 
 /// Gets the Default Modifier for the opcode & addressing modes
 /// @param opcode opcode
@@ -74,16 +83,17 @@ inline _MOD getDefaultModifier(_OP opcode, _AM admo_a, _AM admo_b);
 
 } //// Anonymous Namespace
 
-    /* Parser Functions */
+/* Parser Functions */
 
 /// Creates a label linker to identify the posistions of all labels within an assembly file
 /// @param asm_data a vector containing each line of the assembly file data as a string
 LabelLinker findAsmLabels(AssemblyCode &asm_data);
 
 /// Parses an assembly code string to an asm_instr object
-/// @param str in the format of: <label> [opcode]<.modifier> <mode_a>[op_a],<mode_b>[op_b]
+/// @param line (asm code) in the format of: <label> [opcode]<.modifier> <mode_a>[op_a],<mode_b>[op_b]
 /// @param label_linker used to store and handle labels
-Inst asmStrToInst(std::string &str, LabelLinker &linker);
+/// @param index instruction index (line number) in asm code
+Inst asmStrToInst(std::string &line, LabelLinker &linker, int index);
 
 /// Create a warrior object by parsing assembly code  
 /// @param warrior_name (file)name of the warrior
