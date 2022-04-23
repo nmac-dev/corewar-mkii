99483908ca860e36e4086799a36cead0693b3300
nmac-dev <ntwmac.dev@gmail.com>
2022-04-24 00:52:17 +0100

    Updated: process members and functions

--- main/inc/pcb.hpp
+++ main/inc/pcb.hpp
@@ -7,19 +7,16 @@
 namespace OS {
 
 /// Denotes the status of execution for a pcb
-enum class Status
-{
-    ACTIVE,
-    TERMINATED,
-};
+enum class Status { ACTIVE, TERMINATED, HAULTED, EXIT };
 
 /// Process Control Block: represents a process with an unique ID, status, and program counter
 class PCB
 {
  private:
-    int     p_id;               // unique ID: identifier for the process
-    Status  status;             // current status
-    int     program_counter;    // holds address of next instruction to be executed
+    int     parent_ID;      // UUID of the process's parent
+    int     pc;             // program counter: holds address of next instruction to be executed
+    int     pid;            // unique identifier for the process
+    Status  status;         // current execution status
 
     /// Creates a unique process identifier (starts from 1)
     inline static int createPID()
@@ -31,24 +28,23 @@ class PCB
  /* Functions */
  public:
     /// Creates a process
+    /// @param _parent_ID UUID of the process's parent
     /// @param pc_initial initial program counter index 
-    PCB(int pc_initial);
+    PCB(int _parent_ID, int pc_initial);
     PCB();
  
  /* Getters */
-    inline int    getPID()    const { return p_id; }
-    inline Status getStatus() const { return status; }
+    inline int    getParentID()       const { return parent_ID; }
+    inline int    getPID()            const { return pid;       }
+    inline Status getStatus()         const { return status;    }
+    inline void   setStatus(Status _status) { status = _status; }
 
     /// [setter] Sets the program counters index to the r_value
-    /// @param r_value PCBs updated program counter index
-    void operator<<(int r_value);
+    /// @param _set_pc PCBs updated program counter index
+    void operator<<(int _set_pc);
     /// [getter] Places the program counter index into the r_value
-    /// @param r_value Int to store the PCBs program counter index
-    void operator>>(int &r_value) const;
-    /// Increments PCB's program counter 
-    void operator++();
-    /// Decrements PCB's program counter 
-    void operator--();
+    /// @param get_pc_ Int to store the PCBs program counter index
+    void operator>>(int &get_pc_) const;
 };
 
-} //// namespace OS
+} // namespace OS
--- main/src/pcb.cpp
+++ main/src/pcb.cpp
@@ -5,17 +5,16 @@
 /// Operating System handles: fetch/decode/execute cycle, memory simulator, and warrior processes
 namespace OS {
 
-PCB::PCB(int pc_initial) 
+PCB::PCB(int _parent_ID, int pc_initial)
 {
-    p_id            = createPID();
-    status          = Status::ACTIVE;
-    program_counter = pc_initial;
+    parent_ID = _parent_ID;
+    pc        = pc_initial;
+    pid       = createPID();
+    status    = Status::ACTIVE;
 }
-PCB::PCB(){}
+PCB::PCB() = default;
 
-void PCB::operator<<(int r_value)        { this->program_counter = r_value; }
-void PCB::operator>>(int &r_value) const { r_value = this->program_counter; }
-void PCB::operator++()                   { program_counter++; }
-void PCB::operator--()                   { program_counter--; }
+void PCB::operator<<(int r_value)        { pc      = r_value; }
+void PCB::operator>>(int &r_value) const { r_value = pc;      }
 
-} //// namespace OS
+} // namespace OS
