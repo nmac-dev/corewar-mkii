3e633c77b1cbb7fda15adfa3cf96439fe3038651
nmac-dev <ntwmac.dev@gmail.com>
2022-04-24 02:59:07 +0100

    Added: all opcode execution statements

--- main/inc/core.hpp
+++ main/inc/core.hpp
@@ -1,7 +1,7 @@
-/// Pushes fetch/decode/execute cycles to MARS and selects warrior processes from the scheduler
+/// Executes fetch/decode/execute cycles to MARS and selects warrior processes from the scheduler
 #pragma once
 
-#define CORE_DEBUG
+// #define CORE_DEBUG
 
 #include "mars.hpp"
 #include "scheduler.hpp"
@@ -13,48 +13,61 @@ namespace OS {
 class Core
 {
  private:
-    MARS      simulator;    // memory array simulator
-    Scheduler scheduler;    // manages warrior processes
+    MARS      mars;        // memory array simulator
+    Scheduler scheduler;   // manages warrior processes
+
+    ControlUnit CTRL;      // Control Unit from MARS, used in executiom
+    PCB         PRCS_EXE;  // process executing the instruction
 
  public:
-    struct Report
-    {
-        MARS::CommitLog    commits_log; // log of commits to be made on the simulator
-        Scheduler::PrcsLog process_log; // log from scheduler of current process details
-
-        /// Copies both logs into the report
-        /// @param _commit_log  commit  log from MARS
-        /// @param _process_log process log from Scheduler
-        Report(MARS::CommitLog _commits_log, Scheduler::PrcsLog _process_log);
-        Report();
-    };
+    /// Creates a core to fetch/decode/execute and manage a memory array simulator
+    Core(ASM::WarriorList *warriors, int _seperation, int _cycles, int _processes);
 
  /* Functions */
-    /// Creates a core to fetch/decode/execute and manage a memory array simulator
-    Core(ASM::WarriorList &warriors, int size, int seperation, int n_rounds, int n_cycles);
-
     /// Returns the size of the memory array
-    inline int size()      const { return simulator.size(); }
+    inline int size() const { return mars.size(); }
     /// Returns the number of processes
-    inline int processes() const { return scheduler.processes();  }
+    inline int processes()  { return scheduler.totalPCBs();  }
 
-    /// Commits the next fetch/decode/execute cycle, then returns an operating system report
-    Report nextFDECycle();
+    /// run the next fetch/decode/execute cycle, then returns an operating system report
+    int nextFDECycle();
     
  /* Execute */
  private:
-   /// Pushes a (MOV) commit to the core
-   /// @param _cl commit log to push  
-   void pushReadWrite(MARS::CommitLog &push_cl);
-   /// Pushes a (CMP, SLT, SPL) commit to the core
-   /// @param _cl commit log to push  
-   void pushCompare(MARS::CommitLog &push_cl);
-   /// Pushes a (ADD, SUB, MUL, DIV, MOD) commit to the core
-   /// @param _cl commit log to push  
-   void pushArithmetic(MARS::CommitLog &push_cl);
-   /// Pushes a (JMP, JMZ, JMN, DJN) commit to the core
-   /// @param _cl commit log to push  
-   void pushJump(MARS::CommitLog &push_cl);
+   /// Executes a (DAT, MOV)
+   void execute_ReadWrite();
+   /// Executes a (CMP, SLT, SPL)
+   void execute_Compare(int &jump_val);
+   /// Executes a (ADD, SUB, MUL, DIV, MOD)
+   void execute_Arithmetic();
+   /// Executes a (JMP, JMZ, JMN, DJN)
+   void execute_Jump(int &jump_val);
+
+   /// Arithmatic operation filter: applies the operator to both values and returns the result
+   /// @param operator_char char represnetation of the operation to perform
+   /// @param l_val left value  used as the base
+   /// @param r_val right value to perform operation on
+   inline int arithOpFilter(int l_val, int r_val, char operator_char)
+   {
+      int ret_ = 0;
+      switch (operator_char)
+      {
+      case '+': ret_ = l_val + r_val; break;
+      case '-': ret_ = l_val - r_val; break;
+      case '*': ret_ = l_val * r_val; break;
+      case '/': ret_ = l_val / r_val; break;
+      case '%': ret_ = l_val % r_val; break;
+      default: break;
+      }
+      return ret_;
+   }
+
+   /// Applies the post-increment
+   /// @param _post pointer to be incremented
+   inline void applyPostIncrement(int *_post)
+   {
+       if (_post != 0 && _post != nullptr && _post != NULL) (*_post)++;
+   }
 };
 
-} //// namespace OS
+} // namespace OS
--- main/src/core.cpp
+++ main/src/core.cpp
@@ -5,76 +5,230 @@
 /// Operating System handles: fetch/decode/execute cycle, memory simulator, and warrior processes
 namespace OS {
 
-Core::Report::Report(MARS::CommitLog _commits_log, Scheduler::PrcsLog _process_log)
+// Report::Report(ControlUnit _CTRL, PCB _process)
+// {
+//     CTRL    = _CTRL;
+//     process = _process;
+// }
+// Report::Report() = default;
+
+Core::Core(ASM::WarriorList *_warriors, int _seperation, int _cycles, int _processes) 
 {
-    commits_log = _commits_log;
-    process_log = _process_log;
+    mars      = MARS(_warriors, _seperation);    // must be first (warrior needs core_index)
+    scheduler = Scheduler(_warriors, _cycles, _processes);
 }
-Core::Report::Report(){}
 
-Core::Core(ASM::WarriorList &warriors, int _size, int _seperation, int _rounds, int _cycles) 
+int Core::nextFDECycle()
 {
-    simulator = MARS(warriors, _size, _seperation);     // must be first (warrior needs core_index)
-    scheduler = Scheduler(warriors, _rounds, _cycles);
-}
-
-Core::Report Core::nextFDECycle()
-{
-    MARS::CommitLog    commit_log;              // commits MARS wants to push
-    Scheduler::PrcsLog process_log;             // process executing the instruction
-
     /* Fetch */
-    const int IR_PC = scheduler.peekPC();       // peek the next process program counter
-    ASM::Inst i_register = simulator[IR_PC];    // fetch instruction    
+    int IR_PC;                           // instruction register program counter
+    PRCS_EXE = scheduler.nextProcess();  // fetch next process
+
+    // victory
+    if (PRCS_EXE.getStatus() == Status::EXIT)    return 1;
+    // draw
+    if (PRCS_EXE.getStatus() == Status::HAULTED) return -1;
 
     /* Decode */
-    process_log = scheduler.nextRR(i_register.opcode);  // decode next process,     get log
-    commit_log  = simulator.nextCommit(IR_PC);          // decode next instruction, get log
+    PRCS_EXE >> IR_PC;
+    CTRL     = mars.generateCTRL(IR_PC);  // generate a control unit
+
+    #ifdef CORE_DEBUG
+    static int cycle;
+    printf("\nCore::nextFDECycle:\t Cycle:[%d]\t Warrior:[%d] Index:[%d] Inst:'%s' \n",
+        cycle++, PRCS_EXE.getParentID(), CTRL.EXE.index, CTRL.EXE.inst->toAsmCode().c_str());
+    #endif
 
     /* Execute */
-    switch (commit_log.commit_type)
+    int jump_val = CTRL.EXE.index +1; // value to apply to program counter (next instruction) 
+    switch (CTRL.TYPE.code)
     {
-    case ASM::_OP_TYPE::READWRITE:
-        pushReadWrite(commit_log);
+    case ASM::OPCODE_TYPE::READWRITE:
+        execute_ReadWrite();
         break;
-    /// TDOD:
-    // case ASM::_OP_TYPE::COMPARE:
-    //     pushCompare(commit_log);
-    //     break;
-    // case ASM::_OP_TYPE::ARITHMETIC:
-    //     pushArithmetic(commit_log);
-    //     break;
-    // case ASM::_OP_TYPE::JUMP:
-    //     pushJump(commit_log);
-    //     break;
+    case ASM::OPCODE_TYPE::COMPARISION:
+        execute_Compare(jump_val);
+        break;
+    case ASM::OPCODE_TYPE::ARITHMETIC:
+        execute_Arithmetic();
+        break;
+    case ASM::OPCODE_TYPE::JUMP:
+        execute_Jump(jump_val);
+        break;
     }
+    // check if killed
+    if (PRCS_EXE.getStatus() != Status::TERMINATED)
+    {
+        scheduler.pushJump(PRCS_EXE, jump_val);
+    }
+    
+    applyPostIncrement(CTRL.post_A);  // apply post-increment
+    applyPostIncrement(CTRL.post_B);
 
-    return Core::Report(commit_log, process_log);
+    return 0;
 }
 
-void Core::pushReadWrite(MARS::CommitLog &push_cl)
+void Core::execute_ReadWrite()
 {
-    switch (simulator[push_cl.ir_pc].opcode)
+    ControlUnit::Register &_SRC  = CTRL.SRC,
+                          &_DEST = CTRL.DEST;
+    switch (CTRL.EXE.inst->OP.code)
     {
-    case ASM::_OP::DAT:
-        // already applied to process
+    case ASM::OPCODE::DAT:
+
+        scheduler.killBackProcess(PRCS_EXE);
+        CTRL.SRC.event  = Event::NOOP;
+        CTRL.DEST.event = Event::NOOP;
         break;
-    case ASM::_OP::MOV:
-        switch (push_cl.mod_type)
+        
+    case ASM::OPCODE::MOV:
+
+        switch (CTRL.TYPE.mod)
         {
-        case ASM::_MOD_TYPE::SINGLE:
-            push_cl.dest_A = push_cl.src_A;
-            break;
-        case ASM::_MOD_TYPE::DOUBLE:
-            push_cl.dest_A = push_cl.src_A;
-            push_cl.dest_B = push_cl.src_B;
-            break;
-        case ASM::_MOD_TYPE::FULL:
-            simulator[push_cl.adr_dest] = simulator[push_cl.adr_src];
-            break;
+        case ASM::MOD_TYPE::DOUBLE: *_DEST.B.ptr = _SRC.B.val; // doubles bleed into singles
+        case ASM::MOD_TYPE::SINGLE: *_DEST.A.ptr = _SRC.A.val;        break;
+        case ASM::MOD_TYPE::FULL:   *CTRL.DEST.inst = *CTRL.SRC.inst; break;
         }
-        break;
+        CTRL.SRC.event  = Event::READ;
+        CTRL.DEST.event = Event::WRITE;
     }
 }
 
-} //// namespace OS
+void Core::execute_Compare(int &jump_val)
+{
+    ControlUnit::Register &_SRC  = CTRL.SRC,
+                          &_DEST = CTRL.DEST;
+
+    CTRL.SRC.event  = Event::READ;
+    CTRL.DEST.event = Event::READ;
+    
+    // comparison boolean
+    bool compare_eq = true;
+    switch (CTRL.TYPE.mod)
+    {
+    case ASM::MOD_TYPE::DOUBLE: compare_eq  = _SRC.B.val - _DEST.B.val == 0;
+    case ASM::MOD_TYPE::SINGLE: compare_eq &= _SRC.A.val - _DEST.A.val == 0; break;
+
+    case ASM::MOD_TYPE::FULL:
+        ASM::Inst &src_i  = *CTRL.SRC.inst,
+                  &dest_i = *CTRL.DEST.inst;
+        // entire instruction comparison
+        compare_eq =    src_i.OP.code == dest_i.OP.code  // opcode
+                     && src_i.OP.mod  == dest_i.OP.mod   // modifier
+                     && src_i.A.admo  == dest_i.A.admo   // A admo
+                     && src_i.B.admo  == dest_i.B.admo   // B admo
+                     && src_i.A.val   == dest_i.A.val    // A val
+                     && src_i.B.val   == dest_i.B.val;   // A val
+    }
+
+    switch (CTRL.EXE.inst->OP.code)
+    {
+    case ASM::OPCODE::SEQ: if(compare_eq)          break;   else return; // return skips jump++
+    case ASM::OPCODE::SNE: if(compare_eq == false) break;   else return; 
+
+    case ASM::OPCODE::SLT:
+        switch (CTRL.TYPE.mod)
+        {
+        case ASM::MOD_TYPE::DOUBLE: if (_SRC.B.val < _DEST.B.val) /* bleed */; else return;
+        case ASM::MOD_TYPE::SINGLE: if (_SRC.A.val < _DEST.A.val) break;       else return;
+        }
+    }
+    jump_val++;
+}
+
+void Core::execute_Arithmetic()
+{
+    ControlUnit::Register &_SRC  = CTRL.SRC,
+                          &_DEST = CTRL.DEST;
+
+    CTRL.SRC.event  = Event::READ;
+    CTRL.DEST.event = Event::WRITE;
+
+    // select operator argument
+    char operator_char;
+    switch (CTRL.EXE.inst->OP.code)
+    {
+    case ASM::OPCODE::ADD: operator_char = '+'; break;
+    case ASM::OPCODE::SUB: operator_char = '-'; break;
+    case ASM::OPCODE::MUL: operator_char = '*'; break;
+    case ASM::OPCODE::DIV: operator_char = '/'; break;
+    case ASM::OPCODE::MOD: operator_char = '%'; break;
+    }
+    
+    // check for division by zero
+    if (operator_char == '/' || operator_char == '%')
+    {
+        bool zero_div = false; // true if division by zero
+        switch (CTRL.TYPE.mod)
+        {
+        case ASM::MOD_TYPE::DOUBLE: zero_div = !(_SRC.B.val && _DEST.B.val); // NOT 0
+        case ASM::MOD_TYPE::SINGLE: zero_div = !zero_div && !(_SRC.A.val && _DEST.A.val); break;
+        }
+
+        if (zero_div) // kill process
+        {
+            scheduler.killBackProcess(PRCS_EXE);
+            CTRL.SRC.event = Event::NOOP;
+            CTRL.SRC.event = Event::NOOP;
+            return;
+        }
+    }
+    
+    switch (CTRL.TYPE.mod)
+    {
+    case ASM::MOD_TYPE::DOUBLE: // bleeds into single
+        *_DEST.B.ptr = arithOpFilter(*_DEST.B.ptr, _SRC.B.val, operator_char);
+
+    case ASM::MOD_TYPE::SINGLE: 
+        *_DEST.A.ptr = arithOpFilter(*_DEST.A.ptr, _SRC.A.val, operator_char);
+    }
+}
+
+void Core::execute_Jump(int &jump_val)
+{
+    ASM::OPCODE code = CTRL.EXE.inst->OP.code;
+    ControlUnit::Register &_SRC  = CTRL.SRC,
+                          &_DEST = CTRL.DEST;
+    
+    CTRL.SRC.event  = Event::READ;
+    CTRL.DEST.event = (code == ASM::OPCODE::JMP) ? Event::NOOP 
+                                                 : Event::READ; // JMZ, JMN, DJN
+
+    // comparison boolean: true if target is zero
+    bool compare_zero = true;
+    if (code == ASM::OPCODE::JMZ || code == ASM::OPCODE::JMN)
+    {
+        switch (CTRL.TYPE.mod)
+        {
+        case ASM::MOD_TYPE::DOUBLE: compare_zero  = _DEST.B.val == 0;
+        case ASM::MOD_TYPE::SINGLE: compare_zero &= _DEST.A.val == 0; break;
+        }
+    }
+    /* code */
+    switch (code)
+    {
+    case ASM::OPCODE::JMP: break; // break to set jump value
+    case ASM::OPCODE::JMZ: if ( compare_zero) break;   else return;
+    case ASM::OPCODE::JMN: if (!compare_zero) break;   else return;
+    case ASM::OPCODE::DJN:
+        switch (CTRL.TYPE.mod)
+        {
+        case ASM::MOD_TYPE::DOUBLE:
+            --(*_DEST.B.ptr); /* pre-dec */ 
+            compare_zero  = _DEST.B.val == 0;
+
+        case ASM::MOD_TYPE::SINGLE:
+            --(*_DEST.A.ptr);
+            compare_zero |= _DEST.A.val == 0;
+
+            if(compare_zero) return; // all compared values are zero, skip jump
+        } break;
+    
+    case ASM::OPCODE::SPL:
+        scheduler.addProcess(PRCS_EXE.getParentID(), CTRL.SRC.index); 
+        return;
+    }
+    jump_val = _SRC.index; // only reached by break statement
+}
+
+} // namespace OS
