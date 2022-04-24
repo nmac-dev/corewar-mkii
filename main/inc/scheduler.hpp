25b5d84426f3c75aaed0166d791bb773b7c5a349
nmac-dev <ntwmac.dev@gmail.com>
2022-04-24 02:44:19 +0100

    Added: round robin system and process handling functions

--- main/inc/scheduler.hpp
+++ main/inc/scheduler.hpp
@@ -1,110 +1,99 @@
-//// Handles warrior processes in a round robin system, ensures one process each per cycle
+// Handles warrior processes in a round robin system, ensures one process each per cycle
 #pragma once
 
 // #define SCHEDULER_DEBUG
+// #define SCHEDULER_DEBUG_ADD_PCB
+// #define SCHEDULER_DEBUG_KILL_PCB
+
+#ifdef SCHEDULER_DEBUG
+#define SCHEDULER_DEBUG_ADD_PCB
+#define SCHEDULER_DEBUG_KILL_PCB
+#endif
 
 #include <unordered_map>
-#include "p_queue.hpp"
 #include "pcb.hpp"
+#include "template/queue.hpp"
+#include "template/round_robin.hpp"
 
 /// Operating System handles: fetch/decode/execute cycle, memory simulator, and warrior processes
 namespace OS {
 
-namespace 
+namespace
 {
-using PQ_T      = P_Queue<ASM::Warrior *, PCB>;         // PCB queue with warrior parent
-using PrcsQueue = std::unique_ptr<PQ_T>;                // Queue of scheduled processes
-using Schedules = std::unordered_map<int, PrcsQueue>;   // Hashtable of schedules
+using PrcsQueue = std::unique_ptr<Queue<PCB>>;           // Queue of scheduled processes
+using Schedules = std::unordered_map<int, PrcsQueue>;    // Hashtable of schedules
 }
-
-/// A Round Robin System which manages its position rotation 
-class RRSystem  
-{
- private:
-    int pos;                // position of current round robin sequance
-    int length;             // length of iterations before rotating to 0
-    std::vector<int> uuids; // stores all active warrior's UUIDs
- public:
-
-    /// Create a round robin system using warrior UUIDs
-    /// @param _length of round robin system (before rotating to 0)
-    RRSystem(ASM::WarriorList &warriors, int _length);
-    RRSystem();
-
-    /// Returns the current position
-    inline int index() const { return pos; }
-    /// Return the length of the round robin
-    inline int len() const { return length; }
-    /// Return the UUID at the current round robin position
-    inline int UUID() const { return uuids[pos]; }
-
-    /// Move to next round robin position
-    inline void next() { if (length < ++pos) pos = 0; }
-    /// Remove the input UUID from the round robin system and adjust the length
-    inline void remove(int _uuid) 
-    {
-        for (int i = 0; i < length; i++)
-            if (uuids[i] == _uuid)
-            {
-                while (i < length -1)   // remove UUID (shift remaining)
-                {
-                    uuids[i] = uuids[i +1];
-                    i++; 
-                }
-                uuids.pop_back();
-                length--; // trim sequance
-            }
-    }
-};
 
 /// Manages processes using a queue of PCBs for each warrior
 class Scheduler
 {
  private:
-    int max_rounds,          // max number of rounds before the game is concluded
-        max_cycles;          // max number of cycles before the round has been concluded
-    int t_pcbs;              // total number of pcbs
+    int max_cycles,             // max number of cycles before the round has been concluded
+        max_processes;          // max number of processes a single warrior can create
 
-    Schedules schedules_tbl; // hosts a queue of processes for each warrior
-    RRSystem RR;
+    int cycles;                 // cycles executed
+
+    Schedules schedules_tbl;    // hosts a queue of processes for each warrior
+    RR_T<int> RR;               // A Round Robin System which manages its position rotation
     
  public:
-    /// Process log containing information on the current executing process
-    struct PrcsLog
-    {
-        ASM::Warrior *warrior;  // warrior (parent) of the process
-        PCB    process;         // current process
-        int    n_pcbs;          // number of processes owned
-        Status status;          // current process status
-        bool   has_won;         // true if warrior has won the game
-
-        /// Populates report with a warrior, its number of processes, and default values
-        PrcsLog(ASM::Warrior *_warrior, PCB process, int _n_pcbs);
-        PrcsLog();
-    };
-
- /* Functions */
     /// Create a process scheduler using a PCB queue for each warrior
     /// @param warriors collection of all the warriors
-    /// @param _rounds max number of rounds before the game is concluded
     /// @param _cycles max number of cycles before the round has been concluded
-    Scheduler(ASM::WarriorList &warriors, int _rounds, int _cycles);
+    /// @param _processes max number of processes a single warrior can create
+    Scheduler(ASM::WarriorList *warriors,int _cycles, int _processes);
     Scheduler();
 
+ /* Functions */
     /// Creates a new PCB for a process and sets the program counter to the input core index
-    /// @param parent owner of the process
+    /// @param parent_ID owner of the process
     /// @param pc_initial program counter's initial index, where the first instruction is read
-    void addProcess(ASM::Warrior *parent, int pc_initial);
-    
-    /// Returns the program counter for the process at the front of the schedule
-    int peekPC();
+    void addProcess(int parent_ID, int pc_initial);
+
+    /// Kills the last process added to the warriors pcb queue
+    /// @param _prcs process to kill
+    void killBackProcess(PCB &_prcs);
+
+    /// Adjusts the program counter (relative) of the previous process in the round robin 
+    /// @param _prcs process used to validate adjustment
+    /// @param value added onto the program counter
+    void pushJump(PCB const &_prcs, int value);
 
     /// Returns a report of the current process in the round robin sequance, then move to the next RR
     /// @param opcode evaluates for DAT opcode
-    PrcsLog nextRR(ASM::_OP opcode);
+    PCB nextProcess();
 
+    /// Returns the total number of warriors
+    inline int totalWarriors()  const { return schedules_tbl.size(); }
+    /// Returns the number of a warrior's pcbs
+    /// @param _UUID warrior's UUID
+    inline int warriorPCBs(int _UUID)
+    { 
+        int pcbs = 0;
+        if (schedules_tbl.count(_UUID))
+        {
+            pcbs = schedules_tbl[_UUID].get()->size();
+        }
+        return pcbs;
+    }
     /// Returns the total number of processes
-    inline int processes() const { return t_pcbs; }
+    inline int totalPCBs()
+    { 
+        int start_id = RR.i();
+        int total_ = 0;
+        for (int i = 0; i < RR.len(); i++)
+        {
+            int UUID = RR.i();
+            if(schedules_tbl.count(UUID))
+            {
+                total_ += schedules_tbl[UUID].get()->size();
+            }
+        }
+        // ensure RR position is retained
+        while (RR.i() != start_id) RR.next();
+
+        return total_;
+    }
 };
 
-} //// namespace OS
+} // namespace OS
--- main/src/scheduler.cpp
+++ main/src/scheduler.cpp
@@ -5,131 +5,123 @@
 /// Operating System handles: fetch/decode/execute cycle, memory simulator, and warrior processes
 namespace OS {
 
-Scheduler::PrcsLog::PrcsLog(ASM::Warrior *_warrior, PCB _process, int _n_pcbs)
+Scheduler::Scheduler(ASM::WarriorList *warriors, int _cycles, int _processes)
 {
-    warrior = _warrior;
-    process = _process;
-    n_pcbs  = _n_pcbs;
-    status  = Status::ACTIVE;
-    has_won = false;
-}
-Scheduler::PrcsLog::PrcsLog(){}
+    max_cycles    = _cycles;
+    max_processes = _processes;
+    schedules_tbl.reserve(warriors->size());
 
-RRSystem::RRSystem(ASM::WarriorList &warriors, int _length) 
-{
-    length  = _length;
-    pos     = 0;
-    uuids.reserve(length);
+    cycles = 0;
 
-    // populate
-    while (pos < len())
-    {
-       uuids[pos] = warriors[pos].getUUID();
-       this->next();
-    }
-    this->next(); // reset to 0
-}
-RRSystem::RRSystem() {}
-
-Scheduler::Scheduler(ASM::WarriorList &warriors, int _rounds, int _cycles)
-{
-    max_rounds  = _rounds;
-    max_cycles  = _cycles;
-    t_pcbs      = 0;
-    RR          = RRSystem(warriors, warriors.size());
-    schedules_tbl.reserve(RR.len() -1);
+    std::vector<int> warrior_UUIDs;
+    warrior_UUIDs.reserve(warriors->size());
 
     // create a queue for each warrior
-    for (int i = 0; i < RR.len(); i++)
+    for (int i = 0; i < warriors->size(); i++)
     {
-        ASM::Warrior *warrior_i = &warriors[i];
+        int core_index        = (*warriors)[i].get()->getCoreIndex();
+        warrior_UUIDs.push_back((*warriors)[i].get()->getUUID());
 
         // create a  schedule for the warrior with an initial process
-        schedules_tbl[RR.UUID()] = PrcsQueue(new PQ_T(warrior_i));
-        this->addProcess(warrior_i, warrior_i->getCoreIndex());
-        RR.next();
+        schedules_tbl[warrior_UUIDs[i]] = PrcsQueue(new Queue<PCB>);
+        this->addProcess(warrior_UUIDs[i], (*warriors)[i].get()->getCoreIndex());
     }
-    RR.next();  // reset RR to 0
+    RR = RR_T(warrior_UUIDs);
 
     #ifdef SCHEDULER_DEBUG
-        printf("\nScheduler::Scheduler: initialised with |%d| processes accross |%d| warriors \n",
-                schedules_tbl.size(), RR.len()
-        );
+    printf("\nScheduler::Scheduler: initialised with |%d| processes accross |%d| warriors \n",
+            totalPCBs(), totalWarriors());
     #endif
 }
 Scheduler::Scheduler(){}
 
-void Scheduler::addProcess(ASM::Warrior *parent, int pc_initial)
+void Scheduler::addProcess(int parent_ID, int pc_initial)
 {
-    // validate not null
-    if (!parent || parent == nullptr || parent == NULL) return;
-    
     // validate warrior was pre-loaded in contructor
-    if (schedules_tbl.count(parent->getUUID()))
+    if (schedules_tbl.count(parent_ID))
     {
-        schedules_tbl[parent->getUUID()].get()->push(PCB(pc_initial));
-        t_pcbs++;
-    }
-
-    #ifdef SCHEDULER_DEBUG
-        schedules_tbl[parent->getUUID()].get()->back() >> pc_initial;
-        printf("\nScheduler::addProcess: \t PC:|%d| \t Parent:[%d] '%s' \n",
-                pc_initial, parent->getUUID(), parent->getName().c_str()
-        );
-    #endif
-}
-
-int Scheduler::peekPC()
-{
-    int process_pc;
-    schedules_tbl[RR.UUID()].get()->front() >> process_pc;    
-    return process_pc;
-}
-
-Scheduler::PrcsLog Scheduler::nextRR(ASM::_OP opcode)
-{
-    // process log to return
-    Scheduler::PrcsLog pl_ (
-        schedules_tbl[RR.UUID()].get()->parent(),
-        schedules_tbl[RR.UUID()].get()->pop(),
-        schedules_tbl[RR.UUID()].get()->size()
-    );
-
-    // return if warrior has won
-    if (schedules_tbl.size() == 1)
-    {
-        pl_.has_won = true;
-        return pl_;
-    }
-    
-    // kill process on DAT opcode
-    if (opcode == ASM::_OP::DAT)
-    {
-        pl_.status = Status::TERMINATED;
-        pl_.n_pcbs--;
-        
-        // remove warrior if defeated
-        if (schedules_tbl[RR.UUID()].get()->size() < 1)
+        if (schedules_tbl[parent_ID].get()->size() < max_processes)
         {
-            auto itr = schedules_tbl.find(RR.UUID());
-            schedules_tbl.erase(itr);
-            RR.remove(RR.UUID());
+            schedules_tbl[parent_ID].get()->push(PCB(parent_ID, pc_initial));
         }
     }
-    else // place process to back of queue
+    else printf("ERROR: scheduler failed to add process... UUID|%d| \n", parent_ID);
+
+    #ifdef SCHEDULER_DEBUG_ADD_PCB
+    schedules_tbl[parent_ID].get()->back() >> pc_initial;
+    printf("\nScheduler::addProcess: \t PC:|%d| \t Warrior:[%d] \n",
+            pc_initial, parent_ID );
+    #endif
+}
+
+void Scheduler::pushJump(PCB const &_prcs, int value)
+{
+    // check exists
+    if (schedules_tbl.count(_prcs.getParentID()))
     {
-        schedules_tbl[RR.UUID()].get()->push(pl_.process);
+        // validate matching process 
+        PCB *back_process = schedules_tbl[_prcs.getParentID()].get()->editBack();
+        if (back_process != nullptr)
+        {
+            *back_process << value;
+        }
+        else printf("ERROR: scheduler failed to push Jump... UUID|%d| \n", _prcs.getParentID());
+    }
+}
+
+void Scheduler::killBackProcess(PCB& _prcs)
+{
+    int _UUID = _prcs.getParentID(); // parent UUID
+
+    // check warrior queue exists
+    if (schedules_tbl.count(_UUID))
+    {
+        // validate rollback success
+        schedules_tbl[_UUID].get()->kickBack();
+    
+        _prcs.setStatus(Status::TERMINATED);
+        
+        // remove warrior if defeated
+        if (schedules_tbl[_UUID].get()->size() < 1)
+        {
+            auto itr = schedules_tbl.find(_UUID);
+            schedules_tbl.erase(itr);
+            RR.remove(RR.prev());
+        }
+    }
+    else printf("ERROR: scheduler killBackProcess failed... Warrior:[%d] \n", _UUID);
+
+    #ifdef SCHEDULER_DEBUG_KILL_PCB
+    printf("\nScheduler::killBackProcess:\t Warrior:[%d] \t PCBs:|%d| \n",
+                _UUID, warriorPCBs(_prcs.getParentID()));
+    #endif
+}
+
+PCB Scheduler::nextProcess()
+{
+    PCB prcs_;
+    schedules_tbl[RR.i()].get()->pop(&prcs_);
+
+    // hault process to notify of draw
+    if (cycles++ > max_cycles)
+    {
+        prcs_.setStatus(Status::HAULTED);
+    }
+
+    // process exit, all other warriors have been killed (victory)
+    if (schedules_tbl.size() < 2) 
+    {
+        prcs_.setStatus(Status::EXIT);
     }
 
     #ifdef SCHEDULER_DEBUG
-        printf("\nScheduler::nextRR: \t PID:[%d] PCBs:|%d| \t Parent:[%d] '%s' \n",
-                pl_.process.getPID(), pl_.n_pcbs, 
-                pl_.warrior->getUUID(), pl_.warrior->getName().c_str()
-        );
+    printf("\nScheduler::nextProcess: \t PID:[%d] PCBs:|%d| \t Warrior:[%d] \n",
+            prcs_.getPID(), warriorPCBs(prcs_.getParentID()), prcs_.getParentID());
     #endif
-    
-    RR.next();
-    return pl_;
+
+    schedules_tbl[RR.i()].get()->push(prcs_); // add process to back of queue
+    RR.next();                                // move to next round robin sequance
+    return prcs_;
 }
     
-} //// namespace OS
+} // namespace OS
