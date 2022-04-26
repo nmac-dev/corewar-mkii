2ff95fd5a348a5a931a4a24b51042eb7bc00581b
nmac-dev <ntwmac.dev@gmail.com>
2022-04-26 01:31:37 +0100

    Updated: comments

--- main/warriors/rock.asm
+++ main/warriors/rock.asm
@@ -1,7 +1,8 @@
 ;;; name: Bomber (Rock example)
 ;;; Syntax: <label> [opcode].<modifier> <admo>[A], <admo>[B]
-;;; Line 1: <start>    [add].<ab>          <#>[4],    < >[bmb]
-start   add    #4,    bmb   ;; {parser} -> add.ab #4,  $3     
-        mov    bmb,   @bmb  ;; {parser} -> mov.i  $2,  @2+(#0)
-        jmp    start        ;; {parser} -> jmp    $-2, #0     
-bmb     dat    #0,    #0    ;; {parser} -> dat    #0,  #0     
+
+loop  add.ab  #4,    bomb  ;; add 4 to the bomb's B value
+      mov.i   bomb,  @bomb ;; copy the bomb 12 + (4 * i) addresses ahead
+      jmp     loop         ;; repeat loop indefinitely
+
+bomb  dat     #0,    #12   ;; each loop iteration copies the bomb 4 addresses ahead of the previous
