ccedd5115956010e332a9c7d881dab6743c353dd
nmac-dev <ntwmac.dev@gmail.com>
2022-05-14 02:03:12 +0100

    Edit: code correction

--- main/warriors/rock.asm
+++ main/warriors/rock.asm
@@ -1,8 +1,8 @@
 ;;; name: Bomber (Rock example)
 ;;; Syntax: <label> [opcode].<modifier> <admo>[A], <admo>[B]
 
-loop  add.ab  #4,    bomb  ;; add 4 to the bomb's B value
-      mov.i   bomb,  @bomb ;; copy the bomb 12 + (4 * i) addresses ahead
-      jmp     loop         ;; repeat loop indefinitely
+loop:  add.ab  #4,    bomb  ;; add 4 to the bomb's B value
+       mov.i   bomb,  @bomb ;; copy the bomb 12 + (4 * i) addresses ahead
+       jmp     loop         ;; repeat loop indefinitely
 
-bomb  dat     #0,    #12   ;; each loop iteration copies the bomb 4 addresses ahead of the previous
+bomb:  dat     #0,    #12   ;; each loop iteration copies the bomb 4 addresses ahead of the previous
--- main/warriors/scissors.asm
+++ main/warriors/scissors.asm
@@ -1,14 +1,13 @@
 ;;; name: Scanner (Scissors example)
 ;;; syntax: <label> [opcode].<modifier> <admo>[A], <admo>[B]
 
-scanner  add.ab  #9,      target  ;; adds 9 to the target address
-target   sne.i   d-op,    9       ;; if the target is not a copy of d-op, deploy c-bomb
-         jmp     scanner          ;; no target found, run next scan
+scanner:  add.ab  #3,      target  ;; adds 9 to the target search address
+target:   sne.i   c_bomb,  3       ;; if the target is not a copy of c-bomb, deploy c-bomb
+          jmp     scanner          ;; no target found, run next scan
 
-deploy   mov.i   c-bomb,  >target ;; deploy the cluster bomb at the target address, then post-inc target
-         jmn     deploy,  <reset  ;; loop until reset B == 0
-reset    mov.ab  #3,      #3      ;; set B back to 3
-         jmp     scanner          ;; move back to scanner
+deploy:   mov.i   c_bomb,  >target ;; deploy the cluster bomb at the target address, then post-inc target
+          jmn     deploy,  <reset  ;; loop until reset B == 0
+reset:    mov.ab  #3,      #3      ;; set B back to 3
+          jmp     scanner          ;; move back to scanner
 
-c-bomb   spl     0                ;; cluster bomb for address
-d-op     dat     #0,      #0      ;; Default Core Opcode (not a target)
+c_bomb:   dat     #0       #0      ;; cluster bomb for address
