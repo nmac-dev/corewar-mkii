60dc01ae2a842f9ed060426c6768f222015ebc17
nmac-dev <ntwmac.dev@gmail.com>
2022-05-14 01:44:47 +0100

    Added: C_RAM

---
+++ main/inc/template/c_ram.hpp
@@ -0,0 +1,43 @@
+#include <memory>
+
+/// Circular Random Access Memory: contains an array of T objects
+template<typename T>
+struct C_RAM
+{
+ using RAM_PTR = std::unique_ptr<T[]>; // Unique Ptr to the systems RAM
+ private:
+    int m_size;     // number of memory addresses
+    RAM_PTR m_ram;  // array of memory addresses
+
+ public:
+    /// Creates an array of memory addresses of type <T>
+    C_RAM(int _size) 
+    {
+        m_size = _size;
+        m_ram = RAM_PTR(new T[m_size]);
+    }
+    C_RAM() = default;
+
+    /// Loops the address when outside the bounds of RAM
+    /// @param address RAM address
+    inline int loop_index(int address) const
+    {
+        // loop address until it is within the 
+        while (address < 0 || address >= m_size)
+        {
+            // address to upper core section
+            if (address < 0)  address += m_size;
+            // address to lower core (> core_size)
+            else address -= m_size;
+        }
+        return address;
+    }
+    
+    /// Returns the number of memory addresses
+    inline int size() const { return m_size; };
+
+    /// Access address of RAM's memory array (filters address)
+    T *const operator[](int address) const { return &m_ram[loop_index(address)]; }
+    /// Modify address of RAM's memory array (filters address)
+    T *operator[](int address)             { return &m_ram[loop_index(address)]; }
+};
