974b8c6187a8f46cbbb4dc37e08aa77cef6e2608
nmac-dev <ntwmac.dev@gmail.com>
2022-05-14 02:00:40 +0100

    Updated: decoding functions structure

--- main/inc/mars.hpp
+++ main/inc/mars.hpp
@@ -5,122 +5,85 @@
 
 #include <stdint.h>
 #include <time.h>
-#include "template/ram.hpp"
-#include "asm.hpp"
+#include "ctrl_unit.hpp"
+#include "template/c_ram.hpp"
 
 namespace OS
 {
-namespace 
-{ 
-    using namespace ASM;
-    enum class OPR { A, B };// Used to select operand A or B
-} 
-
-/// Defines event types applied to the core
-enum class Event{ NOOP, READ, WRITE, EXECUTE };
-
-/// ControlUnit: stores the interpretation of the instruction register for the execute phase
-struct ControlUnit
-{
-    /// Type: specifies types for the opcode operation and modifier argument
-    struct Type {
-        OPCODE_TYPE code;  // opcode   operation
-        MOD_TYPE    mod;   // modifier argument
-    };
-    /// Register: Instruction register for the control unit decoding
-    struct Register {
-        int index;                  // absolute Core address
-        Inst::Operation *OP;        // operation pointer
-        Inst::Operand *A, *B;       // operand pointer
-        Event event;                // event applied to the address
-
-        Register(int _index, Inst *_inst);
-        Register();
-    };
-
-    Type     TYPE;
-    Register EXE,         // executing   register from the program counter
-             SRC,         // source      register from MAIN's A operand
-             DEST;        // destination register from MAIN's B operand     
-    int *post_A, *post_B; // points to value to be post-incremented (or nullptr)
-    
-    /// Creates an Instruction ControlUnit (pre-configured for DAT opcode)
-    /// @param _pc  instruction register program counter
-    /// @param _RAM array containing instruction objects
-    ControlUnit(int _pc, RAM_T<Inst> *_RAM);
-    ControlUnit();
-};
 
 /// Memory Array Redcode Simulator (MARS): handles assembly instruction objects within RAM
 class MARS
 {
  private:
-    inline static int core_size = 8000; // number of memory addresses within the core 
-    int min_seperation;                 // min distance between warriors at the start of a round (config.ini) 
+    static int constexpr core_size = 8000;  // number of memory addresses within the core 
+    int ini_min_seperation;                 // min distance between warriors at the start of a round (config.ini) 
 
-    RAM_T<Inst> RAM;               // Array of instruction objects (circular)
+    C_RAM<Inst> RAM;                        // Array of instruction objects (circular)
 
  public:
     /// Initialises the simulator by loading a default asm instruction (dat #0, #0) into every address,
     /// then places each warrior at a random location in accordance with the 'min_seperation' setting
-    /// @param warrior_list collection of warriors to be loaded into the core
-    /// @param _seperation (min_seperation) minimum seperation between warriors in the simulator
-    MARS(WarriorList *warriors, int _seperation);
+    /// @param _warriors collection of warriors to be loaded into the core
+    /// @param _min_seperation minimum seperation between warriors in the simulator
+    MARS(WarriorVec *_warriors, int _min_seperation);
     MARS();
 
  /* Functions */
  private:
     /// Generates a random positive integer using the process clock and xorshift algorithm
-    /// @param maxRange output will not exceed this value
-    uint32_t randomInt(uint32_t maxRange);
+    /// @param _max_range output will not exceed this value
+    uint32_t random_int(uint32_t max_range);
 
-    /// Returns a pointer to either operand A or B values for the index given
-    /// @param index instruction index within the MARS
+    /// Returns a pointer to either operand A or B values for the address given
+    /// @param address instruction address within the MARS
     /// @param select selects operand A or B
-    inline int *selectOperand(int index, OPR select)
+    inline int *fetch_operand(int address, InstField select)
     {
-        int *operand = select == OPR::A ? &RAM[index]->A.val 
-                                        : &RAM[index]->B.val;
+        static int constexpr max_itr = 8,                       // max RAM address loop iterations
+                             itr_limiter = core_size * max_itr; // adjusts value over limit 
 
-        // loop value to arithmatic bounds, stops Integer overflow
-        while (*operand <= -8000) *operand += core_size;    // lower bound
-        while (*operand >=  8000) *operand -= core_size;    // upper bound
+        int *operand = select == InstField::A ? &RAM[address]->A.val    // A
+                                               : &RAM[address]->B.val;   // B
+
+        // loop value to limiter bounds: stops Integer overflow,
+        // and increases RAM indexing performance
+        while (*operand < -(itr_limiter) ) *operand += itr_limiter;    // lower bound
+        while (*operand >   itr_limiter  ) *operand -= itr_limiter;    // upper bound
 
         return operand;
     }
-    
-    /// Swaps the two elements
-    /// @param _X swaped to _Y
-    /// @param _Y swaped to _X
-    template <typename T>
-    inline void swapElements(T &_X, T &_Y)
-    {
-        T swap_i = _X;      // swap index
-        _X       = _Y;
-        _Y       = swap_i;  // = _X
-    }
 
+ public:
     /// Decodes the addressing mode for the input instruction
-    /// @param _CTRL buffer for the decoded results
+    /// @param _ctrl buffer for the decoded results
     /// @param exe_select selects operand A or B
-    ControlUnit::Register decodeAdmo(ControlUnit *_CTRL, OPR const exe_select);
+    Register decode_admo(ControlUnit *_ctrl, InstField const exe_select);
 
     /// Decodes the modifier for the input instruction
-    /// @param _CTRL buffer for the decoded results
-    void decodeModifier(ControlUnit *_CTRL);
- 
- public:
+    /// @param _ctrl buffer for the decoded results
+    void decode_modifier(ControlUnit * _ctrl);
     /// Returns a control unit which has decoded the addressing modes and modifier
     /// @param _pc instruction register program counter to decode
-    ControlUnit generateCTRL(int const _pc);
+    ControlUnit generate_ctrl(int const _pc);
+
+    /// Applies the post-increment if the pointer target exists 
+    /// @param _ctrl control unit containing Registers to be incremented
+    inline void apply_post_inc(ControlUnit & _ctrl)
+    {
+        if (_ctrl.post_A != nullptr)
+            (*_ctrl.post_A)++;
+
+        if (_ctrl.post_B != nullptr)
+            (*_ctrl.post_B)++;
+    }
 
     /// Returns the size of the simulator
-    inline int size() const { return core_size; }
+    static int constexpr size() { return core_size; }
 
-    /// access index of core simulators's memory array
-    Inst &operator[](int index) const;
-    /// modify index of core simulators's memory array
-    Inst &operator[](int index);
-}; // MARS
+    /// access address of core simulators's memory array
+    Inst &operator[](int address) const;
+    /// modify address of core simulators's memory array
+    Inst &operator[](int address);
 
-} // namespace OS
+}; /* ::MARS */
+} /* ::OS */
--- main/src/mars.cpp
+++ main/src/mars.cpp
@@ -4,61 +4,35 @@
 
 namespace OS
 {
-/* ControlUnit */
-ControlUnit::Register::Register(int _index, Inst *_inst)
+MARS::MARS(WarriorVec *_warriors, int _min_seperation)
 {
-    index = _index;
-    OP    = &_inst->OP;
-    A     = &_inst->A; 
-    B     = &_inst->B;
-    event = Event::NOOP; // event applied to the address
-}
-ControlUnit::Register::Register() = default;
-
-ControlUnit::ControlUnit(int _pc, RAM_T<Inst> *RAM)
-{
-    EXE    = Register(_pc, (*RAM)[_pc]);
-    SRC    = Register();  // EXE A
-    DEST   = Register();  // EXE B
-
-    post_A = nullptr;
-    post_B = nullptr;
-
-    TYPE.code = OPCODE_TYPE::READWRITE;
-    TYPE.mod  = MOD_TYPE::SINGLE;
-}
-ControlUnit::ControlUnit() = default;
-
-/* MARS */
-MARS::MARS(WarriorList *warriors, int _seperation)
-{
-    min_seperation = _seperation;
+    ini_min_seperation = _min_seperation;
 
     // populate RAM with (dat #0, #0) asm instructions
-    RAM = RAM_T<Inst>(core_size);
+    RAM = C_RAM<Inst>(core_size);
 
     // place warriors in core at random positions
-    for (int i = 0; i < warriors->size(); i++)
+    for (int i = 0; i < _warriors->size(); i++)
     {
-        Warrior warrior_i = *(*warriors)[i].get();
-        uint32_t rnd_pos  = randomInt(core_size);
+        Warrior warrior_i = *(*_warriors)[i].get();
+        uint32_t rnd_pos  = random_int(core_size);
 
         // validate position meets minimum seperation requirements
         for (int k = 0; k < i; k++)
         {
             // get upper & lower limits for seperation boundry
-            int upper   = RAM.loopIndex(rnd_pos + min_seperation),
-                lower   = RAM.loopIndex(rnd_pos - min_seperation);
-            int w_index = warrior_i.getCoreIndex();
+            int upper   = RAM.loop_index(rnd_pos + ini_min_seperation),
+                lower   = RAM.loop_index(rnd_pos - ini_min_seperation);
+            int w_index = warrior_i.address();
 
             if (w_index > lower && w_index < upper)
             {
                 // failed; generate new position, restart loop
-                rnd_pos = randomInt(core_size);
+                rnd_pos = random_int(core_size);
                 k = 0;
             }
         }
-        (*warriors)[i].get()->setCoreIndex(rnd_pos);
+        (*_warriors)[i].get()->set_address(rnd_pos);
 
         // add each warrior instruction into the core
         for (int j = 0; j < warrior_i.len(); j++)
@@ -68,15 +42,15 @@ MARS::MARS(WarriorList *warriors, int _seperation)
 
         #ifdef MARS_DEBUG
         if (i == 0) printf("\nMARS::MARS: \n");
-        printf("\tAdded |%d| instructions @ index [%d] \t...from: [%d] '%s'\n",
-                warrior_i.len(), (*warriors)[i].get()->getCoreIndex(),
-                warrior_i.getUUID(), warrior_i.getName().c_str());
+        printf("\tAdded |%d| instructions @ address [%d] \t...from: [%d] '%s'\n",
+                warrior_i.len(), (*_warriors)[i].get()->address(),
+                warrior_i.uuid(), warrior_i.name().c_str());
         #endif
     }
 }
 MARS::MARS() = default;
 
-uint32_t MARS::randomInt(uint32_t maxRange)
+uint32_t MARS::random_int(uint32_t max_range)
 {
     static uint32_t repeated_flag; // stores first seed, flag triggers on seed repetition
     static uint32_t seed;          // seed is random and mutates on each function call
@@ -96,154 +70,238 @@ uint32_t MARS::randomInt(uint32_t maxRange)
     seed ^= seed >> 17;
     seed ^= seed << 5;
 
-    return seed % maxRange;
+    return seed % max_range;
 }
 
-ControlUnit::Register MARS::decodeAdmo(ControlUnit *_CTRL, OPR const exe_select)
+Register MARS::decode_admo(ControlUnit *_ctrl, InstField const exe_select)
 {
-    ADMO _admo  = (exe_select == OPR::A) ? _CTRL->EXE.A->admo  // SRC
-                                         : _CTRL->EXE.B->admo; // DEST
-    OPR indirect;
-    int main_i  = _CTRL->EXE.index; // executing instruction index
-    
-    // process addressing mode
-    int pre_dec = 0;                // pre-decrement flag: set to -1 when applied
+    Admo _admo = (exe_select == InstField::A) ? _ctrl->EXE.A->admo  // SRC
+                                              : _ctrl->EXE.B->admo; // DEST
+    InstField indirect_select;
+    int main_i  = _ctrl->EXE.address;
+
+    bool indirect_type = false;
     switch (_admo)
     {
-    case ADMO::IMMEDIATE:
-        // relative address is 0, use stored value
-        break;
-
-    case ADMO::DIRECT:
-        main_i += (*selectOperand(main_i, exe_select));
-        break;
-        
-    /* Indirect */
-    case ADMO::PRE_DEC_A:  pre_dec  = -1;
-    case ADMO::INDIRECT_A:
-    case ADMO::POST_INC_A: indirect = OPR::A; goto CALC_INDIRECT;
-
-    case ADMO::PRE_DEC_B:  pre_dec  = -1;
-    case ADMO::INDIRECT_B:
-    case ADMO::POST_INC_B: indirect = OPR::B; goto CALC_INDIRECT;
-
-    CALC_INDIRECT:
-        main_i += (*selectOperand(main_i, indirect));       // indirect A|B
-        (*selectOperand(main_i, indirect)) += pre_dec;      // apply pre-dec flag
-
-        switch (_admo)
-        {   /* post-inc ptr */
-            case ADMO::POST_INC_A: _CTRL->post_A = selectOperand(main_i, indirect); break;
-            case ADMO::POST_INC_B: _CTRL->post_B = selectOperand(main_i, indirect); break;
+        case Admo::IMMEDIATE:
+        {
+            // relative address is 0
+            break;
         }
-        
-        main_i += (*selectOperand(main_i, indirect));       // indirect
-        break;
+        case Admo::DIRECT:
+        {
+            main_i += (*fetch_operand(main_i, exe_select));
+            break;
+        }
+        /* Indirect */
+        case Admo::PRE_DEC_A:
+        case Admo::POST_INC_A:
+        case Admo::INDIRECT_A:
+        {
+            indirect_select = InstField::A;
+            indirect_type = true;
+            break;
+        }
+        case Admo::PRE_DEC_B:
+        case Admo::POST_INC_B:
+        case Admo::INDIRECT_B:
+        {
+            indirect_select = InstField::B;
+            indirect_type = true;
+            break;
+        }
+        default: break;
     }
-    return ControlUnit::Register(main_i, RAM[main_i]);;
-} // decodeAdmo
 
-void MARS::decodeModifier(ControlUnit *_CTRL)
+    if (indirect_type)
+    {
+        main_i += (*fetch_operand(main_i, exe_select));             // direct
+
+        if (_admo == Admo::PRE_DEC_A || _admo == Admo::PRE_DEC_B)
+        {
+            (*fetch_operand(main_i, indirect_select)) += -1;
+        }
+        if (_admo == Admo::POST_INC_A || _admo == Admo::POST_INC_B)
+        {
+            int *post_inc = fetch_operand(main_i, indirect_select);
+            if(exe_select == InstField::A)
+            {
+                 _ctrl->post_A = post_inc;
+            }
+            else _ctrl->post_B = post_inc;
+        }
+        main_i += (*fetch_operand(main_i, indirect_select));        // indirect
+    }
+    return Register(main_i, RAM[main_i]);;
+} /* decode_admo() */
+
+void MARS::decode_modifier(ControlUnit *_ctrl)
 {
-    OPCODE _code = _CTRL->EXE.OP->code;
-    MOD    _mod  = _CTRL->EXE.OP->mod;
+    Opcode _code = _ctrl->EXE.OP->code;
+    Modifier    _mod  = _ctrl->EXE.OP->mod;
 
-    // ignore modifier
+    // allows -> ModifierType::FULL
+    bool full_type = false;
+    // swap flags
+    bool swap_src  = false,
+         swap_dest = false;
+
+    /* [code] Conditionals */
     switch (_code)
     {
-    case OPCODE::NOP: // ignored
-    case OPCODE::DAT:
-    case OPCODE::JMP:
-    case OPCODE::SPL: return;
-
-    case OPCODE::JMZ: // only change DEST operands on .B
-    case OPCODE::JMN:
-    case OPCODE::DJN: 
-        if      (_mod == MOD::B) goto SWAP_DEST;
-        else if (_mod == MOD::F) _CTRL->TYPE.mod = MOD_TYPE::DOUBLE;
-        return;
+        case Opcode::NOP:
+        case Opcode::DAT:
+        case Opcode::JMP:
+        case Opcode::SPL:
+        {   
+            /* ignore modifier */
+            return;
+        }
+        case Opcode::MOV:
+        case Opcode::SEQ:
+        case Opcode::SNE:
+        {
+            full_type = true;
+            break;
+        }
+        case Opcode::JMZ: // only change DEST operands on .B
+        case Opcode::JMN:
+        case Opcode::DJN:
+        {
+            if (_mod == Modifier::B)
+            {
+                swap_dest = true;
+                _mod = Modifier::A;
+            }
+            else if (_mod == Modifier::F)
+            {
+                _ctrl->TYPE.mod = ModifierType::DOUBLE;
+                return;
+            }
+            break;
+        }
+        default: break;
     }
 
-    // adjust operand value pointers base on mod type
+    /* Apply Modifiers */
     switch (_mod)
     {
-    /* Single [default] */
-    case MOD::A:  break; 
-    case MOD::AB: goto SWAP_DEST;  // only dest
-    case MOD::BA:                  // only src
-    case MOD::B: // SRC A <-> B
-            swapElements(_CTRL->SRC.A, _CTRL->SRC.B);
-            goto SWAP_DEST;
-        
-    /* Double */ 
-    case MOD::F: _CTRL->TYPE.mod  = MOD_TYPE::DOUBLE; break;
-    case MOD::X: _CTRL->TYPE.mod  = MOD_TYPE::DOUBLE; goto SWAP_DEST;
-
-    /* Full */
-    case MOD::I:
-        switch (_code)
+        /* Single [default] */
+        case Modifier::A:  break; 
+        case Modifier::AB:
         {
-        case OPCODE::MOV:
-        case OPCODE::SEQ:
-        case OPCODE::SNE: _CTRL->TYPE.mod = MOD_TYPE::FULL;   break; // MOV, SEQ, SNE (only)
-        default:          _CTRL->TYPE.mod = MOD_TYPE::DOUBLE; break; // else F
-        } break;
-    default: return;
-
-        SWAP_DEST:
-        switch (_mod)
+            swap_dest = true;
+            break;
+        }
+        case Modifier::BA:
         {
-        case MOD::B:
-        case MOD::AB:
-        case MOD::X: // DEST A <-> B
-            swapElements(_CTRL->DEST.A, _CTRL->DEST.B); // PTR
-        } return;
+            swap_src = true;
+            break;
+        }
+        case Modifier::B:
+        {
+            swap_src = swap_dest = true;
+            break;
+        }
+        /* Double */ 
+        case Modifier::F:
+        {
+            _ctrl->TYPE.mod = ModifierType::DOUBLE; 
+            break;
+        }
+        case Modifier::X:
+        {
+            swap_dest = true;
+            _ctrl->TYPE.mod = ModifierType::DOUBLE; 
+            break;
+        }
+        /* Full */ 
+        case Modifier::I:
+        {
+            if (full_type)
+            {
+                 _ctrl->TYPE.mod = ModifierType::FULL;
+            }
+            else _ctrl->TYPE.mod = ModifierType::DOUBLE; // else Modifier::F
+            break;
+        }
+        default: break;
     }
-} // decodeModifier
 
-ControlUnit MARS::generateCTRL(int const _pc)
+    if (swap_src)                   // SRC A <-> B
+    {
+        _ctrl->SRC.swap_operands();
+    }
+    if (swap_dest)                  // DEST A <-> B
+    {
+        _ctrl->DEST.swap_operands();
+    }
+} /* decode_modifier() */
+
+ControlUnit MARS::generate_ctrl(int const _pc)
 {
-    ControlUnit CTRL_ = ControlUnit(_pc, &RAM);
+    ControlUnit ctrl_ ( {_pc, RAM[_pc]} );
 
     // decode addressing modes, get SRC (A) and DEST (B) registers
-    CTRL_.SRC  = decodeAdmo(&CTRL_, OPR::A);
-    CTRL_.DEST = decodeAdmo(&CTRL_, OPR::B);
-                 decodeModifier(&CTRL_);
+    ctrl_.SRC  = decode_admo(&ctrl_, InstField::A);
+    ctrl_.DEST = decode_admo(&ctrl_, InstField::B);
+                 decode_modifier(&ctrl_);
 
     // determine opcode type
-    switch (CTRL_.EXE.OP->code)
+    switch (ctrl_.EXE.OP->code)
     {
-    /* Read/Write */
-    case OPCODE::DAT:
-    case OPCODE::MOV: CTRL_.TYPE.code = OPCODE_TYPE::READWRITE;   break;
-    /* Comparision */
-    case OPCODE::SEQ:
-    case OPCODE::SLT:
-    case OPCODE::SPL: CTRL_.TYPE.code = OPCODE_TYPE::COMPARISION; break;
-    /* Arithmetic */
-    case OPCODE::ADD:
-    case OPCODE::SUB:
-    case OPCODE::MUL:
-    case OPCODE::DIV:
-    case OPCODE::MOD: CTRL_.TYPE.code = OPCODE_TYPE::ARITHMETIC;  break;
-    /* Jump */
-    case OPCODE::JMP:
-    case OPCODE::JMZ:
-    case OPCODE::JMN:
-    case OPCODE::DJN: CTRL_.TYPE.code = OPCODE_TYPE::JUMP;        break;
+        /* System */
+        case Opcode::NOP:
+        case Opcode::DAT:
+        case Opcode::MOV:
+        case Opcode::SPL:
+        {
+            ctrl_.TYPE.code = OpcodeType::SYSTEM;
+            break;
+        }
+        /* Comparision */
+        case Opcode::SEQ:
+        case Opcode::SNE:
+        case Opcode::SLT:
+        {
+            ctrl_.TYPE.code = OpcodeType::COMPARISION;
+            break;
+        }
+        /* Arithmetic */
+        case Opcode::ADD:
+        case Opcode::SUB:
+        case Opcode::MUL:
+        case Opcode::DIV:
+        case Opcode::MOD: 
+        {
+            ctrl_.TYPE.code = OpcodeType::ARITHMETIC;
+            break;
+        }
+        /* Jump */
+        case Opcode::JMP:
+        case Opcode::JMZ:
+        case Opcode::JMN:
+        case Opcode::DJN: 
+        {
+            ctrl_.TYPE.code = OpcodeType::JUMP;
+            break;
+        }
+        default: break;
     }
-    CTRL_.EXE.event = Event::EXECUTE;
 
     #ifdef MARS_DEBUG
-    printf("\nMARS::generateCTRL:\t (EXE)\t |%s| \t Index[%d] \n\t\t\t (SRC)\t '%s' \n\t\t\t (DEST)\t '%s' \n",
-            CTRL_.EXE.inst->toAsmCode().c_str(), CTRL_.EXE.index,
-            CTRL_.SRC.inst->toAsmCode().c_str(), CTRL_.DEST.inst->toAsmCode().c_str());
+    printf("\nMARS::generateCTRL:\t (EXE)  [%d]'%s' \n"
+                          "\n\t\t\t (SRC)  [%d]'%s' \n"
+                          "\n\t\t\t (DEST) [%d]'%s' \n",
+        ctrl_.EXE.address,  RAM[ctrl_.EXE.address]->to_assembly().c_str(),
+        ctrl_.SRC.address,  RAM[ctrl_.SRC.address]->to_assembly().c_str(),
+        ctrl_.DEST.address, RAM[ctrl_.DEST.address]->to_assembly().c_str());
     #endif
 
-    return CTRL_;
-} // nextCommit
+    return ctrl_;
+} /* generate_ctrl() */
 
-Inst &MARS::operator[](int index) const { return *RAM[index]; }
-Inst &MARS::operator[](int index)       { return *RAM[index]; }
+Inst &MARS::operator[](int address) const { return *RAM[address]; }
+Inst &MARS::operator[](int address)       { return *RAM[address]; }
     
-} // namespace OS
+} /* ::OS */
