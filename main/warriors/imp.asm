bf2fad0441c7e1f8be0d749ed8cfe32e360578e2
nmac-dev <ntwmac.dev@gmail.com>
2022-05-19 17:56:36 +0100

    Added: Warrior Imp example

---
+++ main/warriors/imp.asm
@@ -0,0 +1,4 @@
+;;; name: Imp (Single Line Program example)
+;;; syntax: <label> [opcode].<modifier> <admo>[A], <admo>[B]
+
+mov 0, 1  ;; move instruction one space ahead, which is executed on the next turn
