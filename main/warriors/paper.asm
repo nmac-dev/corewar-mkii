3caf6caff0cbd5fea68e6956460dd68e234e08a1
nmac-dev <ntwmac.dev@gmail.com>
2022-05-20 03:12:01 +0100

    Added: Warrior Paper example

--- main/warriors/paper.asm
+++ main/warriors/paper.asm
@@ -1,20 +1,15 @@
-;;; name: Cloner (Paper example)
+;;; name:   Cloner  (Paper example)
 ;;; syntax: <label> [opcode].<modifier> <admo>[A], <admo>[B]
 
-counter:  nop     #0,        #6       ;; counts loop iterations, A = line number, B = lines left to clone
+cloner:  add.f  #1,     1      ;; increase both [mov.i] fields ([A],[B])
+copy:    mov.i  -2,     1023   ;; copy entire program into address 1024 + index
+         seq.ab copy,   split  ;; skip jump once clone is complete
+         jmp    cloner         ;; return to 'cloner:' to copy next line into next address
 
-cloner:  mov.i   }counter,  #987     ;; post-inc line number, clone line to B address
-         jmn     loop,      counter  ;; skip split until counter B == 0
-         spl     #987                ;; create new process once cloner has finished copying
-         mov.i   reset,     counter  ;; reset counter to original state        
-loop:    jmp     cloner,    <counter ;; jump to top of loop and decrement cloner B field
+         mov.i  reset,  1022   ;; reset new process 'copy:'
+split:   spl    1020,   7      ;; create new process when 'copy:' [mov.i] [A] is equal to [spl] [B]
 
-reset:   nop     #0         #7       ;; saved orginal counter state (pre-dec: 7 - 1 = 6)
+fire:    mov.i  3,      -10    ;; after the new process is made, spread a 'fire:' of [dat] codes backwards
+         jmp    fire,   <fire
 
-;; 0 | nop
-;; 1 | mov.i
-;; 2 | jmn
-;; 3 | spl ----------------------
-;; ..                           |
-;; ..                           |
-;; 987 | Clone goes here     <- |
+reset:   mov.i  -2,     1023   ;; never executed, stores original 'copy'
