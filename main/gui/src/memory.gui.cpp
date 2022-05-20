2016e6f31459b0ac70967548d115e07670602cd2
nmac-dev <ntwmac.dev@gmail.com>
2022-05-20 04:04:02 +0100

    Updated: live memory display to include initial program addresses

--- main/gui/src/memory.gui.cpp
+++ main/gui/src/memory.gui.cpp
@@ -18,8 +18,29 @@ void Memory::init(Game *_corewar)
 
 void Memory::reset_display()
 {
+    // set all to default
     for (int i = 0; i < m_cells.size(); i++)
         m_cells[i] = Cell();
+
+    // set initial program
+    for (int plr = 1; plr <= ptr_corewar->players(); plr++)
+    {
+        int adr_ = ptr_corewar->program_address( (Player) plr );
+        int len_ =  ptr_corewar->program_length( (Player) plr );
+
+        // add assembly to cells
+        for (int i = 0; i < len_; i++)
+        {
+            if (adr_ >= m_cells.size())
+                adr_ = 0;
+
+            m_cells[adr_].owner    = (Player) plr;
+            m_cells[adr_].editor   = Player::NONE;
+            m_cells[adr_].event    = OS::Event::EXECUTE;
+            update_cell_assembly(adr_);
+            adr_++;
+        }
+    }
 }
 
 void Memory::update_cells()
@@ -28,72 +49,38 @@ void Memory::update_cells()
 
     int _adr;
     Cell *cell_;
-    // EXE
-    if (ptr_corewar->report().exe.event != OS::Event::NOOP)
-    {
+    /** EXE: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
         _adr  = ptr_corewar->report().exe.address;
         cell_ = &m_cells[_adr];
 
+        cell_->owner = player_;
         cell_->event = ptr_corewar->report().exe.event;
-
-        if (cell_->event == OS::Event::ILLEGAL)
-        {
-            cell_->owner = Player::NONE;
-        }
-        else cell_->owner = player_;
-        cell_->assembly = ptr_corewar->assembly_at(_adr);
-    }
-    // SRC
-    if (ptr_corewar->report().src.event != OS::Event::NOOP)
-    {
+        update_cell_assembly(_adr);
+    /** SRC: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
         _adr  = ptr_corewar->report().src.address;
         cell_ = &m_cells[_adr];
 
         cell_->editor = player_;
         cell_->event  = ptr_corewar->report().src.event;
 
-        if (cell_->event == OS::Event::WRITE)
-            cell_->assembly = ptr_corewar->assembly_at(_adr);
-    }
-    // DEST
-    if (ptr_corewar->report().dest.event != OS::Event::NOOP)
-    {
+        if (_adr != ptr_corewar->report().exe.address)
+            update_cell_assembly(_adr);
+
+    /** DEST: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
         _adr  = ptr_corewar->report().dest.address;
         cell_ = &m_cells[_adr];
 
         cell_->editor = player_;
         cell_->event  = ptr_corewar->report().dest.event;
 
-        if (cell_->event == OS::Event::WRITE)
-            cell_->assembly = ptr_corewar->assembly_at(_adr);
-    }
+        if (_adr != ptr_corewar->report().exe.address)
+            update_cell_assembly(_adr);
 }
 
 void Memory::draw()
     {
      /* Display */
-        static ImVec2 const disp_cell_size   = ImVec2(7.f, 7.f);  // size of a single cell
-        float               disp_cell_distro = 0;                 // distrobution of cells across the displays width
-
-        static ImGuiWindowFlags disp_flags ( 
-            global_abs_flags
-            // extra
-        );
-
-        const ImGuiViewport* viewport = ImGui::GetMainViewport();
-        ImVec2 window_pos  = viewport->Pos;
-        ImVec2 window_size = viewport->Size;
-
-        static ImVec2 const disp_max (1024.f, 540.f);
-        static float constexpr scalar   = 0.65f;
-        bool disp_under_max =  (window_size.x * (scalar + 0.1f)) < disp_max.x;
-        // X Cap
-        if (disp_under_max)
-        {
-            window_size.x *= scalar;
-        }
-        else window_size.x = disp_max.x;
-        
+        static ImGuiWindowFlags disp_flags ( global_window_flags );
 
         /* Save style, then edit */
         ImGuiStyle &edit_style  = ImGui::GetStyle(),
@@ -103,29 +90,18 @@ void Memory::draw()
         edit_style.ItemSpacing    = ImVec2(1.f, 1.f);
 
      /* Cell Distro */
-        float cell_space = disp_cell_size.x + edit_style.ItemSpacing.x;
-        disp_cell_distro = (window_size.x / cell_space); // distro cells across display width
-        
-        // Y Cap
-        if (disp_under_max)
-        {
-            window_size.y = ((float) m_cells.size() / disp_cell_distro) * cell_space;
-            window_size.y += cell_space;
-        }
-        else window_size.y = disp_max.y;
+        // distro cells across display width
+        float disp_cell_distro = ((disp_window_size.x - PAD) 
+                                / (disp_cell_size.x + edit_style.ItemSpacing.x));
 
-        window_size.x += cell_space;
+        ImGui::SetNextWindowSize(disp_window_size);
 
-        ImGui::SetNextWindowPos(window_pos);
-        ImGui::SetNextWindowSize(window_size);
-        ImGui::SetNextWindowViewport(viewport->ID);
-
-        if (ImGui::Begin("Live Memory Execution", NULL, disp_flags))
+        if (ImGui::Begin("Live Memory", NULL, global_window_flags))
         {
             Cell   *cell_;
             ImVec4 color_;
 
-         /* Cells */
+         /* Create Cells */
             int push_;
             int counter_   = 0;
             while ( counter_ < m_cells.size() )
@@ -137,37 +113,32 @@ void Memory::draw()
                     if ( counter_ >= m_cells.size() )
                         break;
 
-                 /* Select Player */
+                 /* Colors Cells */
                     cell_ = &m_cells[counter_];
-                    switch (cell_->event)
+
+                    if (cell_->event == OS::Event::READ || cell_->event == OS::Event::WRITE)
                     {
-                        default:
-                        case OS::Event::EXECUTE:
-                        {
-                            color_ = plr_colors.at(cell_->owner);
-                            // mark executing cell
-                            if (counter_ == ptr_corewar->report().exe.address)
-                                adjust_color(color_);
-                            break;
-                        }
-                        case OS::Event::READ:
-                        case OS::Event::WRITE:
-                        {
-                            color_ = plr_colors.at(cell_->editor);
-                            adjust_color(color_, 0.f, 0.8f);
-                        }
+                        color_ = plr_colors.at(cell_->editor);
+                        adjust_color(color_, 0.f, 0.7f);
                     }
+                    else
+                    {
+                        color_ = plr_colors.at(cell_->owner);
+                        // mark executing cell
+                        if (counter_ == ptr_corewar->report().exe.address)
+                            adjust_color(color_);
+                    }
+
                  /* Create Cell Style */
                     ImGui::PushStyleColor(ImGuiCol_Button,  color_);        push_++;
                     adjust_color(color_);
                     ImGui::PushStyleColor(ImGuiCol_ButtonHovered, color_);  push_++;
-                    adjust_color(color_);
                     ImGui::PushStyleColor(ImGuiCol_ButtonActive, color_);   push_++;
 
                     ImGui::Button(" ", disp_cell_size);
                     if (ImGui::IsItemHovered())
                     {
-                        ImGui::SetTooltip(cell_->assembly.c_str());
+                        ImGui::SetTooltip("%s\n|%d|", cell_->assembly.c_str(), counter_ +1);
                     }
                     ImGui::PopStyleColor(push_);
                     // next cell
