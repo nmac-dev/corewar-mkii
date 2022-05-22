32aec9ead521906c5a3728cf6214b0b9d484554e
nmac-dev <ntwmac.dev@gmail.com>
2022-05-22 21:06:05 +0100

    Updated: Settings to return read-only references

--- main/include/settings.hpp
+++ main/include/settings.hpp
@@ -10,42 +10,45 @@
 /// Singleton class to load & store match settings from a configuration file
 class Settings
 {
+ using ConfigINI = std::unordered_map<std::string, int>;
  private:
-    inline static char const ini_section[]  = "[Match Parameters]";
-    inline static char const ini_filename[] = "corewar.ini";
-    inline static char const ini_comment    = '#';
-    inline static int  const ini_total_vals = 5;
+    static char constexpr ini_section[]  = "[Match Parameters]";
+    static char constexpr ini_filename[] = "corewar.ini";
+    static char constexpr ini_comment    = '#';
+    static int  constexpr ini_total_vals = 5;
+    static inline bool    flag_loaded    = false; // true when the '.ini' file has been loaded
 
     /// .ini setting names
-    inline static char const INI_NAMES[ini_total_vals][32] {
+    static char constexpr ini_names[ini_total_vals][32] {
         "max_rounds",
         "max_cycles",
         "max_processes",
         "max_program_insts",
-        "min_separation"
+        "min_separation",
     };
 
     /// default values for the .ini settings, used if parameters are missing
-    inline static const int INI_DEFAULT_VALUES[ini_total_vals] {
+    static int constexpr ini_defaults[ini_total_vals] {
         3,  // max_rounds
         8,  // max_cycles
         8,  // max_processes
         12, // max_program_insts
         8   // min_separation
-        };
+    };
 
-    /// stores the .ini setting values
-    std::unordered_map<std::string, int> ini_values;
+    /// stores the .ini contents as a hash table
+    static inline ConfigINI config_ini;
 
     /// Constructor is blocked
     Settings(){}
 
+ public:
     /// Uses File_Loader:: to read match_settings.ini, then processes the contents into the hash table
-    inline void load_ini()
+    static void load_ini()
     {
         std::vector<std::string> data = File_Loader::load_file_data(ini_filename, ini_comment);
         std::string name; int value; // Map<key, value>
-        ini_values.clear();
+        config_ini.clear();
 
         // validate data is from correct file
         if (data[0] != ini_section)
@@ -70,62 +73,55 @@ class Settings
                     break;
                 }
             }
-            ini_values.insert(std::make_pair(name, value));
+            config_ini.insert(std::make_pair(name, value));
         }
 
         // add default value for any missing parameters
         for (int i = 0; i < ini_total_vals; i++)
         {
-            if (!ini_values.count(INI_NAMES[i]))
+            if (!config_ini.count(ini_names[i]))
             {
-                ini_values.insert(std::make_pair(INI_NAMES[i], INI_DEFAULT_VALUES[i]));
+                config_ini.insert(std::make_pair(ini_names[i], ini_defaults[i]));
             }
 
             #ifdef SETTINGS_DEBUG
             if (i == 0) printf("\nSettings::load_ini: \n");
-            printf("\t\"%s\" = %d\n", INI_NAMES[i].c_str(), ini_values.at(INI_NAMES[i]));
+            printf("\t\"%s\" = %d\n", ini_names[i], config_ini.at(ini_names[i]));
             #endif
         }
+        flag_loaded = true;;
     }  /* ::load_ini() */
 
- public:
-    /// Settings instance access method
-    static Settings& get()
+ private:
+    /// Always called before fetching setting to ensure file is loaded
+    static inline void fetch()
     {
-        static Settings match_settings;
-        
         // flag to ensure the settings .ini file has been loaded before any values are used
-        static int flag_loaded;
         if (!flag_loaded)
-        {
-            match_settings.load_ini();
-            flag_loaded++;
-        }
-        return match_settings;
+            load_ini();
     }
 
+ public:
     /// Copy creation method deleted
     Settings(Settings const &)        = delete;
     /// Assignment operator method deleted
     void operator= (Settings const &) = delete;
 
-    
-    /// ini_filename of the .ini configuration file containing the settings values
-    inline char const *file_name() const { return get().ini_filename; }
+ /* Parameters */
 
     /// Max number of rounds before the game is concluded
-    inline int max_rounds()        const { return get().ini_values.at(INI_NAMES[0]); }
+    static inline int const &max_rounds()        { fetch(); return config_ini.at(ini_names[0]); }
 
     /// Max number of cycles before the round has been concluded
-    inline int max_cycles()        const { return get().ini_values.at(INI_NAMES[1]); }
+    static inline int const &max_cycles()        { fetch(); return config_ini.at(ini_names[1]); }
 
     /// Max number of processes a single program can create
-    inline int max_processes()     const { return get().ini_values.at(INI_NAMES[2]); }
+    static inline int const &max_processes()     { fetch(); return config_ini.at(ini_names[2]); }
 
     /// Max instructions a program can consist of
-    inline int max_program_insts() const { return get().ini_values.at(INI_NAMES[3]); }
+    static inline int const &max_program_insts() { fetch(); return config_ini.at(ini_names[3]); }
 
     /// Min distance between programs at the start of a round
-    inline int min_separation()    const { return get().ini_values.at(INI_NAMES[4]); }
+    static inline int const &min_separation()    { fetch(); return config_ini.at(ini_names[4]); }
 
 }; /* ::Settings */
