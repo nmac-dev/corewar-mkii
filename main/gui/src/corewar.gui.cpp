6837f059f836eb3a2a0dd80f595aac4583539e68
nmac-dev <ntwmac.dev@gmail.com>
2022-05-23 01:04:38 +0100

    Added: Control Panel to GUI render loop

--- main/gui/src/corewar.gui.cpp
+++ main/gui/src/corewar.gui.cpp
@@ -1,9 +1,9 @@
 /// Corewar GUI using 'Dear ImGui'
 
-#include "corewar.gui.hpp"
+#include "imgui_required.hpp"
+#include "control_panel.gui.hpp"
 
 int main() {
-
 /** GLFW:GLAD:SETUP: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     /* Init Window */
     glfwInit();
@@ -12,7 +12,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(1366, 768, "Corewar", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(1440.f, 556.f, "Corewar", nullptr, nullptr);
 
     /* Errors */
     if (window == nullptr) {
@@ -31,16 +31,13 @@ int main() {
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
 
-    /* Set Config */ // must be set before backend is  initialised
+    /* Set Config */ // must be set before backend is initialised
     ImGuiIO& io = ImGui::GetIO(); (void)io;
 
-    io.IniFilename = "imgui.ini";
-    io.ConfigFlags |= Corewar::GUI::global_config_flags;
+    io.IniFilename = ".gui.ini";
+    io.ConfigFlags |= Corewar::GUI::GLOBAL_CONFIG_FLAGS;
 
-    //io.ConfigViewportsNoAutoMerge = true;
-    //io.ConfigViewportsNoTaskBarIcon = true;
-
-    /* Tweaks for viewport */
+    /* Tweaks for Viewport */
     ImGuiStyle& style = ImGui::GetStyle();
     if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
     {
@@ -51,48 +48,14 @@ int main() {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(GLSL_VERSION);
 
-/** COREWAR:GAME:SETUP: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
+/** COREWAR:GUI:SETUP: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    /* Game Instance */
     using namespace Corewar;
-    static Game corewar_;
+    static Game COREWAR_GAME;
 
-    char constexpr rock[]        = "rock.asm",      // test warriors
-                   paper[]       = "paper.asm",
-                   scissors[]    = "scissors.asm",
-                   imp[]         = "imp.asm";
-
-    WarriorFiles filenames = 
-    { 
-        rock,
-        rock,
-        rock,
-        paper,
-        paper,
-        paper,
-        scissors,
-        scissors,
-        scissors,
-        scissors,
-        // imp,
-    };
-
-    // check game status
-    switch (corewar_.new_game(filenames))
-    {
-        case State::ERR_INI:       // bad/missing 'corewar.ini'
-            printf("\n Error: bad/missing 'corewar.ini' (EXIT_FAILURE)");
-            return EXIT_FAILURE;
-        
-        case State::ERR_WARRIORS:  // missing warrior
-            printf("\n Error: selected warriors missing or contain invalid assembly (EXIT_FAILURE)");
-            return EXIT_FAILURE;
-    }
-    static int round_tracker = corewar_.round();    // used to notify GUI
-
-/** COREWAR:GUI:INIT: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     /* Main Viewport */
     static ImGuiWindowFlags main_window_flags ( 
-        GUI::global_window_flags
+        GUI::GLOBAL_WINDOW_FLAGS
         // extra
         | ImGuiWindowFlags_NoTitleBar
         | ImGuiWindowFlags_NoBringToFrontOnFocus
@@ -100,38 +63,9 @@ int main() {
         | ImGuiWindowFlags_NoBackground
     );
 
-    /* OS */
-    GUI::Memory::init(&corewar_);
-
 /** LOOP:START: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-    while (!glfwWindowShouldClose(window)) {
-
-        
-    /** GAME:TURN:START: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-        
-        corewar_.next_turn();
-
-        if (corewar_.state() != Corewar::State::RUNNING)
-        {   
-            Player winner_ = corewar_.match_winner();
-            if (winner_!= Player::NONE)
-            {
-                printf("\n MATCH Winner! . . . %s \n", corewar_.player_to_str(winner_));
-            }
-            printf("\n Corewar: NOT Running . . . Game Reset \n");
-            corewar_.reset_game();
-        }
-
-        if (round_tracker != corewar_.round())
-        {
-            round_tracker = corewar_.round();
-           
-            Player winner_ = corewar_.round_winner();
-            printf("\n Round Winner! . . . %s \n", corewar_.player_to_str(winner_));
-            GUI::Memory::reset_display();
-        }
-
-    /** GAME:TURN:END: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    while (!glfwWindowShouldClose(window))
+    { 
         glfwPollEvents();
 
         ImGui_ImplOpenGL3_NewFrame();
@@ -140,32 +74,39 @@ int main() {
         ImGui::NewFrame();
     /** FRAME:START: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-
-    //  /* Fullscreen */
+     /* Fullscreen */
         static ImGuiViewport const *viewport = ImGui::GetMainViewport();
         ImGui::SetNextWindowPos(viewport->Pos);
         ImGui::SetNextWindowSize(viewport->Size);
         ImGui::SetNextWindowViewport(viewport->ID);
+        ImGui::SetNextWindowBgAlpha(0.0f);
 
         static int main_push = 2;
         ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
         ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
 
-    //  /* Main Display */
+     /* Main Display */
         ImGui::Begin("Corewar", NULL, main_window_flags);       ImGui::PopStyleVar(main_push);
         {
-         /* Set Multi-Viewport */
          /* Set Docking */
             if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
             {
                ImGuiID dockspace_id = ImGui::GetID("Main Dockspace");
-               ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), GUI::global_dockspace_flags);
+               ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), GUI::GLOBAL_DOCKSPACE_FLAGS);
                ImGuiDockNode* Node  = ImGui::DockBuilderGetNode( dockspace_id );
-               Node->LocalFlags    |= GUI::global_dockspace_flags_private_override;
+               Node->LocalFlags    |= GUI::GLOBAL_DOCKSPACE_FLAGS_PRIVATE;
+               
+               io.IniFilename = NULL; // stop '.ini' overwrite once loaded
             }
-         /** COREWAR:GUI: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-            GUI::Memory::update_cells();
-            GUI::Memory::draw();
+
+         /** COREWAR:GUI:DRAW:START: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+            if (GUI::ControlPanel::init_flag() == false)
+            {
+                GUI::ControlPanel::init(&COREWAR_GAME);
+            }
+            /* Draw GUI */
+            GUI::ControlPanel::draw();
+         /** COREWAR:GUI:DRAW:END: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
         }
         ImGui::End();
 
