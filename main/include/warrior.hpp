f2e5cff7f3f6c147d14487e9247819e08cbdf393
nmac-dev <ntwmac.dev@gmail.com>
2022-05-23 01:09:11 +0100

    Added: pointer to operating system program uuid for Warrior class

--- main/include/warrior.hpp
+++ main/include/warrior.hpp
@@ -22,28 +22,28 @@ enum class Player : int
     P8,     // Yellow
     P9,     // Purple
 };
+using Score = int;
 
 /// Contains information for a corewar warrior (warrior)
 class Warrior
 {
  private:
-    OS::UUID    m_id;     // unique id
-    std::string m_name;   // name (without directory)
-    Player      m_player; // enum representing the warrior
+    OS::UUID const *m_id;     // unique id
+    std::string     m_name;   // name (without directory)
+    Player          m_player; // enum representing the warrior
 
     /* Game Stats */
-    int  m_prcs,          // number of running procrsses
-         m_wins,          // total wins
-         m_draws;         // Total draws
+    int   m_prcs;         // number of running procrsses
+    Score m_score;        // current number of points scored
  public:
 
     /// Create new warrior info
     /// @param _uuid   warrior's uuid
     /// @param _name   warrior's name
     /// @param _player enum representing warrior
-    Warrior(OS::UUID _uuid, std::string _name, Player _player)
+    Warrior(OS::UUID const &_uuid, std::string _name, Player _player)
     {
-        m_id     = _uuid;
+        m_id     = &_uuid;
         m_name   = _name;
         m_player = _player;
         clear_stats();
@@ -53,47 +53,52 @@ class Warrior
  /* Utility */
 
     /// Returns warriors unique ID
-    inline OS::UUID const id()      const { return m_id;        }
+    inline OS::UUID const &id()      const { return *m_id;       }
 
     /// Returns warrior's name
-    inline std::string const name() const { return m_name;      }
+    inline std::string const &name() const { return m_name;      }
 
     /// Returns enum of the warrior's play
-    inline Player const player()    const { return m_player;    }
-    
-    /// Returns current number of processes
-    inline int const prcs()         const { return m_prcs;      }
-    
-    /// Returns total wins
-    inline int const wins()         const { return m_wins;      }
+    inline Player const &player()    const { return m_player;    }
 
-    /// Returns total draws
-    inline int const draws()        const { return m_draws;     }
+    /// Returns current number of processes
+    inline int const &prcs()         const { return m_prcs;      }
+    
+    /// Returns current number of points scored
+    inline Score const &score()      const { return m_score;      }
 
     /// Returns false: when all the warrior's processes have been killed
-    inline bool const is_alive()    const { return m_prcs > 0;  }
+    inline bool const is_alive()     const { return m_prcs > 0;  }
+
+    /// Returns the player as a string
+    inline std::string player_string() const 
+    {
+        return std::string ("P" + std::to_string( (int) m_player ));
+    }
+    
+    inline std::string to_string() const
+    {
+        return std::string( player_string() + " '" + name() + "'" );
+    }
 
     /// Updates the processes using the Game's scheduler
-    inline void update_prcs(OS::Scheduler &_sched)
-    {
-         m_prcs = _sched.processes(m_id);
-    }
+    inline void update_prcs(OS::Scheduler &_sched) { m_prcs = _sched.processes(*m_id); }
 
     /// Updates game stats based on the final report
     inline void update_game_results(OS::Report &_report)
     {
         switch (_report.status)
         {
-            case OS::Status::EXIT:    m_wins++;   break;
-            case OS::Status::HAULTED: m_draws++;  break;
+            case OS::Status::EXIT:    m_score += 2;  break;  // wins are worth double
+            case OS::Status::HAULTED: m_score++;     break;
             default: break;
         }
     }
 
     /// Clears the warriors stats ready for a new game
-    inline void clear_stats()
+    inline void clear_stats() 
     { 
-        m_prcs = m_draws = m_wins = 0;
+        m_prcs = m_score = 0;
     }
 };
 
